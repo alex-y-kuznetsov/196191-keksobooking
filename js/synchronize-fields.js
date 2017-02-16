8b9ab2237f69c7533b68e70a6bed286534750f09
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-02-16 17:25:29 +0500

    Трэвис

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,8 +3,8 @@
 window.synchronizeFields = function (field1, field2, array1, array2, property) {
   field1.addEventListener('change', function () {
     field2[property] = array2[array1.indexOf(field1.value)];
-  })
+  });
   field2.addEventListener('change', function () {
     field1[property] = array1[array2.indexOf(field2.value)];
-  })
+  });
 };
