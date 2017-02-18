1745717673ba8727c327b24924ca07981217f5f3
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-02-18 16:10:37 +0500

    Перенесен функционал открытия карточки объявления

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -20,15 +20,7 @@ window.initializePins = (function () {
       }
     };
 
-    // Проверка на тип события
-    var eventType = function (event) {
-      return (event.keyCode && event.keyCode === ENTER_KEY_CODE) || event.type === 'click';
-    };
-
-    // Показ карточки объявления
-    var pinMap = document.querySelector('.tokyo__pin-map');
-    var dialog = document.querySelector('.dialog');
-
+    // Деактивация пинов
     var pinDeactivate = function () {
       var pinActive = document.querySelector('.pin--active');
       if (pinActive) {
@@ -36,36 +28,5 @@ window.initializePins = (function () {
         toggleAria(pinActive);
       }
     };
-
-    var openDialog = function (event) {
-      pinDeactivate();
-      event.target.closest('.pin').classList.add('pin--active');
-      toggleAria(event.target.closest('.pin'));
-      toggleAria(dialog);
-      dialog.style.display = 'block';
-    };
-
-    var openDialogEventListener = function (event) {
-      if (event.target.closest('.pin') && eventType(event)) {
-        openDialog(event);
-      }
-    };
-
-    pinMap.addEventListener('click', openDialogEventListener);
-    pinMap.addEventListener('keydown', openDialogEventListener);
-
-    // Скрытие карточки объявления
-    var dialogClose = document.querySelector('.dialog__close');
-
-    var closeDialogEventListener = function (evt) {
-      if (eventType(evt)) {
-        dialog.style.display = 'none';
-        toggleAria(dialog);
-        pinDeactivate();
-      }
-    };
-
-    dialogClose.addEventListener('click', closeDialogEventListener);
-    dialogClose.addEventListener('keydown', closeDialogEventListener);
   };
 })();
