eff3ba9f9b456e0811707ba978a0fe087ea1247d
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-02-21 22:49:25 +0500

    Синхронизация полей работает без цикла for

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,21 +3,13 @@
 window.synchronizeFields = (function () {
   return function (field1, field2, array1, array2, cb) {
     field1.addEventListener('change', function () {
-      for (var i = 0; i < array1.length; i++) {
-        if (field1.value === array1[i]) {
-          if (typeof cb === 'function') {
-            cb(field2, array2[i]);
-          }
-        }
+      if (typeof cb === 'function') {
+        cb(field2, array2[array1.indexOf(field1.value)]);
       }
     });
     field2.addEventListener('change', function () {
-      for (var i = 0; i < array2.length; i++) {
-        if (field2.value === array2[i]) {
-          if (typeof cb === 'function') {
-            cb(field1, array1[i]);
-          }
-        }
+      if (typeof cb === 'function') {
+        cb(field1, array1[array2.indexOf(field2.value)]);
       }
     });
   };
