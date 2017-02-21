b3d109fc830d18bf83beb578a7c50fa29a836c77
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-02-21 19:15:54 +0500

    Трэвис

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -6,19 +6,19 @@ window.synchronizeFields = (function () {
       for (var i = 0; i < array1.length; i++) {
         if (field1.value === array1[i]) {
           if (typeof cb === 'function') {
-          cb(field2, array2[i]);
+            cb(field2, array2[i]);
           }
         }
       }
-    })
+    });
     field2.addEventListener('change', function () {
       for (var i = 0; i < array2.length; i++) {
         if (field2.value === array2[i]) {
           if (typeof cb === 'function') {
-          cb(field1, array1[i]);
+            cb(field1, array1[i]);
           }
         }
       }
-    })
+    });
   };
 })();
