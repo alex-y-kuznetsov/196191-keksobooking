0091406526972003b837b99133eaec580f8e6b7a
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-02-21 19:09:33 +0500

    Добавил коллбэк в синхронизацию поля

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,12 +1,24 @@
 'use strict';
 
 window.synchronizeFields = (function () {
-  return function (field1, field2, array1, array2, property) {
+  return function (field1, field2, array1, array2, cb) {
     field1.addEventListener('change', function () {
-      field2[property] = array2[array1.indexOf(field1.value)];
-    });
+      for (var i = 0; i < array1.length; i++) {
+        if (field1.value === array1[i]) {
+          if (typeof cb === 'function') {
+          cb(field2, array2[i]);
+          }
+        }
+      }
+    })
     field2.addEventListener('change', function () {
-      field1[property] = array1[array2.indexOf(field2.value)];
-    });
+      for (var i = 0; i < array2.length; i++) {
+        if (field2.value === array2[i]) {
+          if (typeof cb === 'function') {
+          cb(field1, array1[i]);
+          }
+        }
+      }
+    })
   };
 })();
