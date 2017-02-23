c8c572ba47e63d4c84343410c156e684270f3add
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-02-23 21:04:11 +0500

    Проблемы с отрисовкой пинов

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -2,6 +2,39 @@
 
 window.initializePins = (function () {
   var pinMap = document.querySelector('.tokyo__pin-map');
+  var DATA_URL = 'https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data';
+  var similarApartments = [];
+
+  // Поиск похожих объявлений
+  var getData = function () {
+    window.load(DATA_URL, function (data) {
+      similarApartments = data;
+    });
+  };
+
+  // Отрисовка клона
+  var drawClonePin = function (data) {
+    var templateElement = document.querySelector('#pin-template');
+    var elementToClone = templateElement.content.querySelector('.element-to-clone');
+    var newElement = elementToClone.cloneNode(true);
+    var avatar = newElement.querySelector('img');
+
+    avatar.src = data.author.avatar;
+    newElement.style.left = data.location.x + 'px';
+    newElement.style.top = data.location.y + 'px';
+    newElement.setAttribute('tabindex', 0);
+
+    return newElement;
+  };
+
+  //Отрисовка похожих объявлений
+  var drawSimilarApartments = function () {
+    var slicedApartmentsArray = similarApartments.slice(0, 3);
+
+    slicedApartmentsArray.forEach(function (item) {
+      pinMap.appendChild(drawClonePin(item));
+    });
+  };
 
   // Очистка пинов
   var clearPins = function () {
