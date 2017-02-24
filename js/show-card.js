6bbd2315be6ba1cde4c2feacffaa3c9f26286ce3
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-02-24 15:33:52 +0500

    Добавлена отрисока диалога (не получает данные)

--- js/show-card.js
+++ js/show-card.js
@@ -3,21 +3,61 @@
 window.showCard = (function () {
 
   // Показ карточки объявления
-  var dialog = document.querySelector('.dialog');
+  var tokyo = document.querySelector('.tokyo');
   var onDialogClose = null;
+  var dialogTemplate = document.querySelector('#dialog-template');
+  var dialogToClone = dialogTemplate.content.querySelector('.dialog');
+  var newDialogElement = dialogToClone.cloneNode(true);
+  var dialogImage = newDialogElement.querySelector('img');
+  var dialogTitle = newDialogElement.querySelector('.lodge__title');
+  var dialogAddress = newDialogElement.querySelector('.lodge__address');
+  var dialogPrice = newDialogElement.querySelector('.lodge__price');
+  var dialogType = newDialogElement.querySelector('.lodge__type');
+  var dialogRoomsAndGuests = newDialogElement.querySelector('.lodge__rooms-and-guests');
+  var dialogCheckIn = newDialogElement.querySelector('.lodge__checkin-time');
+  var dialogFeatures = newDialogElement.querySelector('.lodge__features');
+  var dialogDescription = newDialogElement.querySelector('.lodge__description');
+  var dialogPhotos = newDialogElement.querySelector('.lodge__photos');
 
-  var openDialog = function () {
-    window.utils.toggleAria(dialog);
-    dialog.style.display = 'block';
+  newDialogElement.style.display = 'none';
+
+  var openDialog = function (data) {
+    window.utils.toggleAria(newDialogElement);
+    newDialogElement.style.display = 'block';
+
+    dialogImage.setAttribute('src', data.author.avatar);
+    dialogImage.setAttribute('alt', 'Avatar');
+    dialogTitle.innerText = data.offer.title;
+    dialogAddress.innerText = data.offer.address;
+    dialogPrice.innerText = data.offer.price;
+    dialogType.innerText = data.offer.type;
+    dialogRoomsAndGuests.innertext = data.offer.rooms + 'комнаты для ' + data.offer.guests + 'гостей.';
+    dialogCheckIn.innertext = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+    dialogDescription.innerText = data.offer.description;
+
+    data.offer.features.forEach(function (item) {
+      var featureElement = document.createElement('span');
+      featureElement.classList.add('feature__image');
+      featureElement.classList.add('feature__image--' + item);
+      dialogFeatures.appendChild(featureElement);
+    });
+    data.offer.photos.forEach(function (item) {
+      var imageElement = document.createElement('img');
+      imageElement.src = item;
+      dialogPhotos.appendChild(imageElement);
+    });
+
+    tokyo.appendChild(newDialogElement);
+
   };
 
   // Скрытие карточки объявления
-  var dialogClose = document.querySelector('.dialog__close');
+  var dialogClose = newDialogElement.querySelector('.dialog__close');
 
   var closeDialog = function (evt) {
     if (window.utils.eventType(evt)) {
-      dialog.style.display = 'none';
-      window.utils.toggleAria(dialog);
+      newDialogElement.style.display = 'none';
+      window.utils.toggleAria(newDialogElement);
       if (typeof onDialogClose === 'function') {
         onDialogClose();
       }
@@ -27,8 +67,8 @@ window.showCard = (function () {
   dialogClose.addEventListener('click', closeDialog);
   dialogClose.addEventListener('keydown', closeDialog);
 
-  return function (cb) {
-    openDialog();
+  return function (data, cb) {
+    openDialog(data);
     onDialogClose = cb;
   };
 })();
